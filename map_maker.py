1a2432e9aadc441854a6e99704541af30632d2ac
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2012-12-03 20:50:21 -0500

    Updated map maker to use csv format

--- map_maker.py
+++ map_maker.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python
 
-import simplekml, pickle
+import simplekml, csv
 from googlemaps import GoogleMaps
+
+
+def utf_8_encoder(unicode_csv_data):
+    for line in unicode_csv_data:
+        yield line.encode('utf-8')
+
+def unicode_csv_reader(unicode_csv_data, **kwargs):
+    # csv.py doesn't do Unicode; encode temporarily as UTF-8:
+    csv_reader = csv.reader(utf_8_encoder(unicode_csv_data),
+                            dialect=dialect, **kwargs)
+    for row in csv_reader:
+        # decode UTF-8 back to Unicode, cell by cell:
+        yield [unicode(cell, 'utf-8') for cell in row]
 
 # Replace for your own key
 # https://developers.google.com/maps/documentation/javascript/tutorial#api_key
@@ -9,21 +22,22 @@ gmaps = GoogleMaps('AIzaSyAmEvPWMafJFafvXGR-ampYOLQVSWmP5xM')
 
 kml = simplekml.Kml()
 
-# read python dict back from the file
-pkl_file = open('tweet_user_data.pkl', 'rb')
-users = pickle.load(pkl_file)
-pkl_file.close()
+# CSV format
+# username, tweet, location, profile pic, timestamp, number of tweets, number of followers, numbers of following
 
-for user, attributes in users.items():
-    try:
-        pnt = kml.newpoint()
-        pnt.name = attributes['name']
-        pnt.description = attributes['description']
-        address = attributes['location']
-        lat, lng = gmaps.address_to_latlng(address)
-        pnt.coords = [(lng, lat)]
-    except:
-        pass
+with open('tweets.csv', 'rb') as csvfile:
+    twitter_reader = unicode_csv_reader(csvfile, delimiter=',', quotechar='"')
+    for row in twitter_reader:
+        try:
+            pnt = kml.newpoint()
+            pnt.name = row[0]
+            pnt.description = row[1]
+            address = row[3]
+            lat, lng = gmaps.address_to_latlng(address)
+            pnt.coords = [(lng, lat)]
+            pnt.timestamp.when= row[4]
+        except:
+            pass
 
 # Save the KML
 kml.save("gangnam.kml")
