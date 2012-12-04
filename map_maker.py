4b7cb597b5ec4acb9317928a6b49bd67d9ae200d
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2012-12-03 22:22:35 -0500

    Updates all around

--- map_maker.py
+++ map_maker.py
@@ -8,10 +8,9 @@ def utf_8_encoder(unicode_csv_data):
     for line in unicode_csv_data:
         yield line.encode('utf-8')
 
-def unicode_csv_reader(unicode_csv_data, **kwargs):
+def unicode_csv_reader(unicode_csv_data, dialect=csv.excel, **kwargs):
     # csv.py doesn't do Unicode; encode temporarily as UTF-8:
-    csv_reader = csv.reader(utf_8_encoder(unicode_csv_data),
-                            dialect=dialect, **kwargs)
+    csv_reader = csv.reader(utf_8_encoder(unicode_csv_data), dialect=dialect, **kwargs)
     for row in csv_reader:
         # decode UTF-8 back to Unicode, cell by cell:
         yield [unicode(cell, 'utf-8') for cell in row]
@@ -25,19 +24,22 @@ kml = simplekml.Kml()
 # CSV format
 # username, tweet, location, profile pic, timestamp, number of tweets, number of followers, numbers of following
 
-with open('tweets.csv', 'rb') as csvfile:
-    twitter_reader = unicode_csv_reader(csvfile, delimiter=',', quotechar='"')
+style = simplekml.Style()
+style.iconstyle.icon.href = "http://localhost:8000/gangnam-style.svg"
+style.iconstyle.scale = 3
+
+with open('demo.csv', 'rb') as csvfile:
+    twitter_reader = unicode_csv_reader(csvfile)
     for row in twitter_reader:
-        try:
-            pnt = kml.newpoint()
-            pnt.name = row[0]
-            pnt.description = row[1]
-            address = row[3]
-            lat, lng = gmaps.address_to_latlng(address)
-            pnt.coords = [(lng, lat)]
-            pnt.timestamp.when= row[4]
-        except:
-            pass
+        pnt = kml.newpoint()
+        pnt.style = style
+        pnt.name = row[0]
+        pnt.description = row[1]
+        style.labelstyle.scale = 2  # Make the text twice as big
+        address = row[2]
+        lat, lng = gmaps.address_to_latlng(address)
+        pnt.coords = [(lng, lat)]
+        pnt.timestamp.when= row[4]
 
 # Save the KML
 kml.save("gangnam.kml")
